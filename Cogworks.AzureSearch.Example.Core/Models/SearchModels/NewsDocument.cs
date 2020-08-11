25c30bfcaab714a95cf09d6aab6faaf337e6163e
Adrian Ochmann <adrian.ochmann@outlook.com>
2020-08-11 14:45:31 +0200

    chore: example of markup index model

--- Cogworks.AzureSearch.Example.Core/Models/SearchModels/NewsDocument.cs
+++ Cogworks.AzureSearch.Example.Core/Models/SearchModels/NewsDocument.cs
@@ -1,6 +1,12 @@
-﻿namespace Cogworks.AzureSearch.Example.Core.Models.SearchModels
+﻿using Cogworks.AzureSearch.Models;
+using Microsoft.Azure.Search;
+using System.ComponentModel.DataAnnotations;
+
+namespace Cogworks.AzureSearch.Example.Core.Models.SearchModels
 {
-    public class NewsDocument : SearchModel
+    public class NewsDocument : IAzureModel
     {
+        [Key, IsFilterable, IsSearchable]
+        public string ContentId { get; set; }
     }
 }
