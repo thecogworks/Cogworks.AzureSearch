d7cf2b53014a3bc07af547290cb736b7e6184370
Adrian Ochmann <adrian.ochmann@outlook.com>
2020-08-14 13:49:00 +0200

    chore: remove example code

--- src/Cogworks.AzureSearch.Example.Core/Constants/Models/EventDocumentConstants.cs
+++
@@ -1,9 +0,0 @@
-﻿using Cogworks.AzureSearch.Example.Core.Models.SearchModels;
-
-namespace Cogworks.AzureSearch.Example.Core.Constants.Models
-{
-    internal class EventDocumentConstants
-    {
-        public const string Name = nameof(EventDocument.Name);
-    }
-}
