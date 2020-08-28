4239ec4996d6403c7f3d7fd2dad93bf7654c9346
Adrian Ochmann <adrian.ochmann@outlook.com>
2020-08-28 11:16:19 +0200

    feat: added Umbraco extension methods for Ioc registrations

--- src/Cogworks.AzureSearch.Umbraco.IocExtension/Extensions/RegisterExtensions.cs
+++ src/Cogworks.AzureSearch.Umbraco.IocExtension/Extensions/RegisterExtensions.cs
@@ -1,12 +1,22 @@
 ﻿using Cogworks.AzureSearch.Umbraco.IocExtension.Builders;
+using LightInject;
 using Umbraco.Core.Composing;
+using LightInjectAzureSearchBuilder = Cogworks.AzureSearch.LightInject.IocExtension.Builders.AzureSearchBuilder;
 
 namespace Cogworks.AzureSearch.Umbraco.IocExtension.Extensions
 {
     public static class RegisterExtensions
     {
-        public static AzureSearchBuilder RegisterAzureSearch(this IRegister composingRegister)
+        public static AzureSearchBuilder RegisterAzureSearch(this Composition composingRegister)
             => new AzureSearchBuilder(composingRegister)
+                .RegisterRepositories()
+                .RegisterIndexes()
+                .RegisterSearchers()
+                .RegisterInitializers()
+                .RegisterWrappers();
+
+        public static LightInjectAzureSearchBuilder RegisterAzureSearch(this IRegister composingRegister)
+            => new LightInjectAzureSearchBuilder(composingRegister.Concrete as ServiceContainer)
                 .RegisterRepositories()
                 .RegisterIndexes()
                 .RegisterSearchers()
