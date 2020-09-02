ee70b7171b0d14e12848cedeac1cca85faadb293
Adrian Ochmann <adrian.ochmann@outlook.com>
2020-09-02 15:20:29 +0200

    chore: simplified umbraco extension method.

--- src/Cogworks.AzureSearch.Umbraco.IocExtension/Extensions/RegisterExtensions.cs
+++ src/Cogworks.AzureSearch.Umbraco.IocExtension/Extensions/RegisterExtensions.cs
@@ -1,26 +1,21 @@
 ﻿using Cogworks.AzureSearch.Builder;
+using Cogworks.AzureSearch.LightInject.IocExtension.Extensions;
 using Cogworks.AzureSearch.Umbraco.IocExtension.Builders;
 using LightInject;
 using Umbraco.Core.Composing;
-using LightInjectAzureSearchBuilder = Cogworks.AzureSearch.LightInject.IocExtension.Builders.AzureSearchBuilder;
 
 namespace Cogworks.AzureSearch.Umbraco.IocExtension.Extensions
 {
     public static class RegisterExtensions
     {
         public static IAzureSearchBuilder RegisterAzureSearch(this IRegister composingRegister, bool useConcreteContainer = false)
-            => !useConcreteContainer
-                ? new AzureSearchBuilder(composingRegister)
+            => useConcreteContainer
+                ? (composingRegister.Concrete as IServiceContainer).RegisterAzureSearch() as IAzureSearchBuilder
+                : new AzureSearchBuilder(composingRegister)
                     .RegisterRepositories()
                     .RegisterIndexes()
                     .RegisterSearchers()
                     .RegisterInitializers()
-                    .RegisterWrappers()
-                : new LightInjectAzureSearchBuilder(composingRegister.Concrete as ServiceContainer)
-                    .RegisterRepositories()
-                    .RegisterIndexes()
-                    .RegisterSearchers()
-                    .RegisterInitializers()
-                    .RegisterWrappers() as IAzureSearchBuilder;
+                    .RegisterWrappers();
     }
 }
