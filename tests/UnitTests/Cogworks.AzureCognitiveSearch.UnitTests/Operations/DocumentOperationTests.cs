ef0882c952bea25b0ab4061f977ce5702ea62537
Adrian Ochmann <adrian.ochmann@outlook.com>
2021-05-11 12:45:20 +0200

    feat: added more test for document operation tests

--- tests/UnitTests/Cogworks.AzureCognitiveSearch.UnitTests/Operations/DocumentOperationTests.cs
+++ tests/UnitTests/Cogworks.AzureCognitiveSearch.UnitTests/Operations/DocumentOperationTests.cs
@@ -1,12 +1,14 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 using AutoFixture;
-using Azure;
 using Azure.Search.Documents.Models;
+using Cogworks.AzureCognitiveSearch.Exceptions.DocumentsExceptions;
 using Cogworks.AzureCognitiveSearch.Interfaces.Operations;
 using Cogworks.AzureCognitiveSearch.Repositories;
 using Cogworks.AzureCognitiveSearch.UnitTests.Models;
 using NSubstitute;
+using NSubstitute.ExceptionExtensions;
 using Xunit;
 
 namespace Cogworks.AzureCognitiveSearch.UnitTests.Operations
@@ -63,6 +65,71 @@ namespace Cogworks.AzureCognitiveSearch.UnitTests.Operations
                     Assert.True(succeedItem.Succeeded);
                     Assert.Equal(200, succeedItem.StatusCode);
                     Assert.Equal("Successfully adding or updating document.", succeedItem.Message);
+                    Assert.Contains(testDocuments, item => item.Id == succeedItem.ModelId);
+                });
+        }
+
+        [Theory]
+        [InlineData(100)]
+        [InlineData(500)]
+        [InlineData(1000)]
+        public async Task Should_Fail_When_AddOrUpdateDocumentsPartiallyFail(int documentsCount)
+        {
+            // Arrange
+            var testDocuments = Enumerable.Range(0, documentsCount)
+                .Select(_ => Fixture.Create<TestDocumentModel>())
+                .ToArray();
+
+
+            var indexResults = testDocuments
+                .Select((document, index) => new
+                {
+                    Document = document,
+                    Succeded = index % (documentsCount / 2) == 0
+                })
+                .Select(item => SearchModelFactory.IndexingResult(
+                    key: item.Document.Id,
+                    errorMessage: !item.Succeded
+                        ? "Internal Unit Error."
+                        : string.Empty,
+                    succeeded: item.Succeded,
+                    status: item.Succeded
+                        ? 200
+                        : 404))
+                .ToList();
+
+            var documentIndexResult = SearchModelFactory.IndexDocumentsResult(indexResults);
+
+            _ = DocumentOperationWrapper
+                .IndexAsync(Arg.Any<IndexDocumentsBatch<TestDocumentModel>>())
+                .Returns(new TestResponse<IndexDocumentsResult>(documentIndexResult));
+
+            // Act
+            var result = await _azureDocumentOperation.AddOrUpdateDocumentsAsync(testDocuments);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.False(result.Succeeded);
+            Assert.NotEmpty(result.SucceededDocuments);
+            Assert.NotEmpty(result.FailedDocuments);
+            Assert.All(
+                result.SucceededDocuments,
+                succeedItem =>
+                {
+                    Assert.True(succeedItem.Succeeded);
+                    Assert.Equal(200, succeedItem.StatusCode);
+                    Assert.Equal("Successfully adding or updating document.", succeedItem.Message);
+                    Assert.Contains(testDocuments, item => item.Id == succeedItem.ModelId);
+                });
+
+            Assert.All(
+                result.FailedDocuments,
+                succeedItem =>
+                {
+                    Assert.False(succeedItem.Succeeded);
+                    Assert.Equal(404, succeedItem.StatusCode);
+                    Assert.Equal("Failed adding or updating document.", succeedItem.Message);
+                    Assert.Equal("Internal Unit Error.", succeedItem.InnerMessage);
                     Assert.Contains(testDocuments, item => item.Id == succeedItem.ModelId);
                 });
         }
@@ -205,6 +272,94 @@ namespace Cogworks.AzureCognitiveSearch.UnitTests.Operations
                     Assert.Equal("Failed removing document.", failedItem.Message);
                     Assert.Contains(testDocuments, item => item.Id == failedItem.ModelId);
                 });
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(100)]
+        [InlineData(500)]
+        [InlineData(1000)]
+        public async Task Should_ThrowDomainException_When_IssueWith_TryAddOrUpdateDocuments(int documentsCount)
+        {
+            // Arrange
+
+            var testDocuments = Enumerable.Range(0, documentsCount)
+                .Select(_ => Fixture.Create<TestDocumentModel>())
+                .ToArray();
+
+            _ = DocumentOperationWrapper
+                .IndexAsync(Arg.Any<IndexDocumentsBatch<TestDocumentModel>>())
+                .Throws(_ => new AddOrUpdateDocumentException("Test Error", Fixture.Create<Exception>()));
+
+            // Assert
+            var domainException = await Assert.ThrowsAsync<AddOrUpdateDocumentException>(async () =>
+                await _azureDocumentOperation.AddOrUpdateDocumentsAsync(testDocuments));
+
+            Assert.NotNull(domainException);
+            Assert.Equal("Test Error", domainException.Message);
+            Assert.NotNull(domainException.InnerException);
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(100)]
+        [InlineData(500)]
+        [InlineData(1000)]
+        public async Task Should_Not_ThrowDomainException_When_No_IssueWith_TryAddOrUpdateDocuments(int documentsCount)
+        {
+            // Arrange
+            _ = IndexOperationWrapper.ExistsAsync(Arg.Any<string>())
+                .Returns(true);
+
+            // Act
+            var domainExceptionResult = await Record.ExceptionAsync(() => Should_AddOrUpdateDocuments(documentsCount));
+
+            // Assert
+            Assert.Null(domainExceptionResult);
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(100)]
+        [InlineData(500)]
+        [InlineData(1000)]
+        public async Task Should_ThrowDomainException_When_IssueWith_TryRemoveDocuments(int documentsCount)
+        {
+            // Arrange
+
+            var testDocuments = Enumerable.Range(0, documentsCount)
+                .Select(_ => Fixture.Create<TestDocumentModel>())
+                .ToArray();
+
+            _ = DocumentOperationWrapper
+                .IndexAsync(Arg.Any<IndexDocumentsBatch<TestDocumentModel>>())
+                .Throws(_ => new AddOrUpdateDocumentException("Test Error", Fixture.Create<Exception>()));
+
+            // Assert
+            var domainException = await Assert.ThrowsAsync<RemoveDocumentException>(async () =>
+                await _azureDocumentOperation.TryRemoveDocumentsAsync(testDocuments));
+
+            Assert.NotNull(domainException);
+            Assert.Equal("Test Error", domainException.Message);
+            Assert.NotNull(domainException.InnerException);
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(100)]
+        [InlineData(500)]
+        [InlineData(1000)]
+        public async Task Should_Not_ThrowDomainException_When_No_IssueWith_TryRemoveDocuments(int documentsCount)
+        {
+            // Arrange
+            _ = IndexOperationWrapper.ExistsAsync(Arg.Any<string>())
+                .Returns(true);
+
+            // Act
+            var domainExceptionResult = await Record.ExceptionAsync(() => Should_Succeeded_When_TryRemoveDocuments(documentsCount));
+
+            // Assert
+            Assert.Null(domainExceptionResult);
         }
     }
 }
