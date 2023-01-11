18a74c4dd7ab5e60ef577010dcd3057497604885
Adrian Ochmann <adrian.ochmann@outlook.com>
2023-01-11 11:14:46 +0100

    feat: added Autofac IoC provider project

---
+++ src/IoC.Providers/Cogworks.AzureSearch.IoC.Autofac/tests/UnitTests/Cogworks.AzureSearch.IoC.Autofac.UnitTests/Models/NotRegistredTestDocumentModel.cs
@@ -0,0 +1,17 @@
+﻿using Azure.Search.Documents.Indexes;
+using Cogworks.AzureSearch.Models;
+
+namespace Cogworks.AzureSearch.IoC.Autofac.UnitTests.Models
+{
+    public class NotRegisteredTestDocumentModel : IModel
+    {
+        [SimpleField(IsKey = true, IsFilterable = true)]
+        [SearchableField()]
+        public string Id { get; set; }
+
+        [SimpleField(IsFilterable = true)]
+        [SearchableField()]
+
+        public string Name { get; set; }
+    }
+}
