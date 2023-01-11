db7b3e1db1a628376882eb24acb9ff8888db6171
Adrian Ochmann <adrian.ochmann@outlook.com>
2023-01-11 11:08:14 +0100

    feat: added Microsoft IoC provider project

---
+++ src/IoC.Providers/Cogworks.AzureSearch.IoC.Microsoft/src/Extensions/BuilderExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Cogworks.AzureSearch.IoC.Microsoft.Builders;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Cogworks.AzureSearch.IoC.Microsoft.Extensions
+{
+    public static class BuilderExtensions
+    {
+        public static ContainerBuilder RegisterAzureSearch(this IServiceCollection serviceCollection)
+            => new ContainerBuilder(serviceCollection)
+                .RegisterRepositories()
+                .RegisterIndexes()
+                .RegisterSearchers()
+                .RegisterInitializers()
+                .RegisterWrappers()
+                .RegisterOperations();
+    }
+}
