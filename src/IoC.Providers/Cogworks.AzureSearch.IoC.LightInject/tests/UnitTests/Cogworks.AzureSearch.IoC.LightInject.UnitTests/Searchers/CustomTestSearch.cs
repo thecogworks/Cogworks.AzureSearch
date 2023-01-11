6ef24a5736acadbbce5d7b2018703c82f267613c
Adrian Ochmann <adrian.ochmann@outlook.com>
2023-01-11 11:18:20 +0100

    feat: added LightInject IoC provider project

---
+++ src/IoC.Providers/Cogworks.AzureSearch.IoC.LightInject/tests/UnitTests/Cogworks.AzureSearch.IoC.LightInject.UnitTests/Searchers/CustomTestSearch.cs
@@ -0,0 +1,27 @@
+﻿using Cogworks.AzureSearch.Interfaces.Searches;
+using Cogworks.AzureSearch.IoC.LightInject.UnitTests.Models;
+using Cogworks.AzureSearch.Searchers;
+
+namespace Cogworks.AzureSearch.IoC.LightInject.UnitTests.Searchers
+{
+    public interface ICustomTestSearch
+    {
+        void SomeCustomSearchExample();
+    }
+
+    public class CustomTestSearch : BaseDomainSearch<FirstTestDocumentModel>, ICustomTestSearch
+    {
+        public CustomTestSearch(ISearcher<FirstTestDocumentModel> search) : base(search)
+        {
+        }
+
+        public void SomeCustomSearchExample()
+        {
+            // Start of custom filters/logic
+            // ...
+            // End of custom filters
+
+            //  _ = base.Search("test", new SearchParameters());
+        }
+    }
+}
